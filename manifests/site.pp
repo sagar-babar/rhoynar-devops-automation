505c0cdee345452d957788fa840955d4940d40e5
sagarinitcron <sagar@initcron.org>
2016-07-25 23:14:56 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,16 +4,6 @@ node 'agent1.example.com'{
 }
 
 node 'agent2.example.com'{
-class { 'gerrit': }
-class { 'jenkins::slave':
-                 masterurl => 'http://check1.example.com:8080',
-                 ui_user => 'adminuser',
-                 ui_pass => 'adminpass',
-                }
-
-}
-
-node 'agent3.example.com'{
 
 	class { 'postgresql::server': } ->
 	postgresql::server::db { 'jira':
@@ -31,6 +21,15 @@ node 'agent3.example.com'{
 	class { 'jira':
  		javahome    => '/usr/java/jdk1.8.0_51',
  	}	
+}
+
+node 'agent3.example.com'{
+class { 'jenkins::slave':
+                 masterurl => 'http://check1.example.com:8080',
+                 ui_user => 'adminuser',
+                 ui_pass => 'adminpass',
+                } ->
+class { 'gerrit': }
 }
 node default{
 	user {'test':
