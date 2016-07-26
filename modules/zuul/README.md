e99b90d40402a0732656cc19d8cce86233021398
root <root@jessie.raw>
2016-07-26 15:58:10 +0000

    ok

--- modules/zuul/README.md
+++
@@ -1,5 +0,0 @@
-# OpenStack Zuul Module
-
-## Overview
-
-TODO: Quick overview
--- modules/zuul/spec/acceptance/fixtures/postconditions.pp
+++
@@ -1,7 +0,0 @@
-exec { 'starting zuul server':
-  command => '/etc/init.d/zuul start',
-}
-
-exec { 'starting zuul merger':
-  command => '/etc/init.d/zuul-merger start'
-}
