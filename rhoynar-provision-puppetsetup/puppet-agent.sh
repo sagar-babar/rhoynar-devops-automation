08c693d8524af13f4f234362501c4f46d143ff09
sagarinitcron <sagar@initcron.org>
2016-07-27 21:47:02 +0530

    Update puppet-agent.sh

--- rhoynar-provision-puppetsetup/puppet-agent.sh
+++ rhoynar-provision-puppetsetup/puppet-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This script designed for setup of puppet agent in debian jessie#
-
+export DEBIAN_FRONTEND=noninteractive
 hostname1="$1"
 masterhost="$4   $3.example.com $3 puppet"
 agenthost="$2    $1.example.com $1"
@@ -26,5 +26,10 @@ echo "certname = $certname" >> /etc/puppet/puppet.conf
 
 # Starting puppet-agent
 service puppet restart
-#puppet agent --test
 puppet agent --enable
+sleep 3
+echo "Kindly wait $1 is fetching and applying catlogs from puppet master..!! DO NOT EXIT.."
+#echo `puppet agent --test` >> /dev/null 2>&1
+puppet agent --test 
+
+exit 0
