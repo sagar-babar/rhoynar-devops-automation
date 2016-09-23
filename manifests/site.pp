212dff14e744a3b0bd884143e95cc3aff86e20f4
MeenachiSundaram V <meenachisundaram.v@gmail.com>
2016-09-24 03:18:55 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -35,40 +35,40 @@ node 'agent2.example.com'{
 
 node 'agent3.example.com'{
 		class { 'jenkins::slave':
-    		masterurl => 'http://agent2.example.com:8080',
-    		ui_user => '',
-    		ui_pass => '',
+	    		masterurl => 'http://agent2.example.com:8080',
+	    		ui_user => '',
+	    		ui_pass => '',
 		} ->
 
 		class { 'gerrit':
-		canonicalweburl => 'http://agent3.example.com:8090/',
-                httpd_hostname  => 'agent3.example.com',
-                httpd_port      => '8090',
+			canonicalweburl => 'http://agent3.example.com:8090/',
+		        httpd_hostname  => 'agent3.example.com',
+		        httpd_port      => '8090',
 		}
 }
 
 node 'agent4.example.com'{
 		class { 'zuul':
-				gerrit_server => 'agent3.example.com',
-				gerrit_user => 'testuser',
-				gearman_server => 'agent4.example.com',
-				zuul_url =>'http://agent4.example.com/p',
-				gerrit_baseurl => 'http://agent3.example.com:8090',
-				zuul_ssh_private_key => '/var/lib/zuul/ssh/id_rsa',
+			gerrit_server => 'agent3.example.com',
+			gerrit_user => 'testuser',
+			gearman_server => 'agent4.example.com',
+			zuul_url =>'http://agent4.example.com/p',
+			gerrit_baseurl => 'http://agent3.example.com:8090',
+			zuul_ssh_private_key => '/var/lib/zuul/ssh/id_rsa',
 		}->
 		class { 'zuul::launcher':
-				ensure => 'running',
+			ensure => 'running',
 		}->
 		class {	'zuul::merger':
-				ensure => 'running',
+			ensure => 'running',
     		}->
 		class { 'zuul::server':
-				ensure => 'running',
-				layout_dir => '/etc/zuul/layout/',
+			ensure => 'running',
+			layout_dir => '/etc/zuul/layout/',
 		}~>
 		exec { 'zuul-restart':
-				command => '/etc/init.d/zuul restart && /etc/init.d/zuul-merger restart',
-				refreshonly => true,
+			command => '/etc/init.d/zuul restart && /etc/init.d/zuul-merger restart',
+			refreshonly => true,
 		}
 		class { 'zuul::known_hosts' :
 		known_hosts_content => '',
@@ -76,15 +76,15 @@ node 'agent4.example.com'{
 		class { 'zuul::params':
         		project_name => 'final-test',
         		job1_name => 'dev-unit-tests-before-merge-prod',
-						job2_name => 'dev-unit-test-patchset',
-						job3_name => 'code-review-rejection',
-						job4_name => 'copy-dev-merge-to-prod',
+			job2_name => 'dev-unit-test-patchset',
+			job3_name => 'code-review-rejection',
+			job4_name => 'copy-dev-merge-to-prod',
         		jenkins_url => 'http://agent2.example.com:8080',
         		jenkins_username => '',
         		jenkins_password => '',
         		node => 'master',
         		zuul_cloner_url => 'http://agent3.example.com:8090',
-						git_push_url => 'agent3.example.com:8090',
+			git_push_url => 'agent3.example.com:8090',
 		}
 }
 
